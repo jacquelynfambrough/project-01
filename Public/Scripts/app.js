f316d672188bfcf21f85b8ff3d2bf723a03d1d1c
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-11 07:22:56 -0700

    put ajax call added

--- Public/Scripts/app.js
+++ Public/Scripts/app.js
@@ -3,33 +3,41 @@
 $(document).ready(function() {
 console.log("Sanity check! app.js running");
 
-
+//getting data from seed
   $.get('/api/stories').success(function (stories) {
     stories.forEach(function(story) {
       renderStories(story);
 
     });
   });
-
+//submitting data on new story form
   $("#newPub").on('submit', function(event){
      event.preventDefault();
      var formData = $(this).serialize();
-        console.log('formData',
-         formData);
-         console.log("CALLING POST..");
         $.post('/api/stories', formData, function(story) {
           console.log('story after POST', story);
           renderStories(story);  //render the server's response
         });
-        console.log("ENDING POST...");
         $(this).trigger("reset");
       });
+//updating data in modal window
+  $("#myEditModal").on('submit', function(event){
+      event.preventDefault();
+      var formData = $(this).serialize();
+        $.put('/api/stories/:id_story', formData, function (editedStory){
+          console.log('story after PUT', editedStory){
+          renderStories(editedStory);
+        }
+          }
+        });
 
 
     });//end doc ready
 
 
 
+
+}
 
 function renderStories(storyList){
   console.log("rendering story", storyList);
