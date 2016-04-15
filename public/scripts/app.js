cf3b06eb5edf183fde9fe147208eff9b4c6a2e66
tgaff <tgaff@alumni.nd.edu>
2016-04-15 11:11:59 -0700

    fix indentation in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,16 @@
 //app.js
 
 $(document).ready(function() {
-console.log("Sanity check! app.js running");
-//getting data from seed
+  console.log("Sanity check! app.js running");
+  //getting data from seed
 
   $.get('/api/stories').success(function (stories) {
     stories.forEach(function(story) {
       renderStories(story);
     });
   });
-//submitting data on new story form
+
+  //submitting data on new story form
   $("#newPub").on('submit', function(event){
     event.preventDefault();
     var formData = $(this).serialize();
@@ -21,30 +22,28 @@ console.log("Sanity check! app.js running");
     $(this).trigger("reset");
   });
 
- $('#publish').on('click', '.btn-danger',handleDeleteStoryClick);
- function handleDeleteStoryClick(e){
-   var storyId = $(this).closest('button').data("story-id");
-   console.log(storyId);
-   $.ajax({
+  $('#publish').on('click', '.btn-danger',handleDeleteStoryClick);
+  function handleDeleteStoryClick(e){
+    var storyId = $(this).closest('button').data("story-id");
+    console.log(storyId);
+    $.ajax({
       method:'DELETE',
       url:'/api/stories/' + storyId,
       success:handleDeleteonSuccess
     });
-};
+  }
 
   function handleDeleteonSuccess(data) {
-   var deletedStory = data._id;
-   console.log('removing ', deletedStory, 'from page');
-   $('div[data-story-id=' + deletedStory + ']').remove();
+    var deletedStory = data._id;
+    console.log('removing ', deletedStory, 'from page');
+    $('div[data-story-id=' + deletedStory + ']').remove();
   }
 
 
-
-
-function renderStories(storyList){
-  var source = $('#published-template').html();
-  var template = Handlebars.compile(source);
-  var newHtml = template(storyList);
-  $('#publish').prepend(newHtml);
-};
+  function renderStories(storyList){
+    var source = $('#published-template').html();
+    var template = Handlebars.compile(source);
+    var newHtml = template(storyList);
+    $('#publish').prepend(newHtml);
+  }
 });// end doc ready
