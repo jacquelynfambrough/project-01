0f41b052ffcefafe58e34ff93b51de721570f878
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-23 05:14:25 -0700

    fixed bug

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ ROUTES
 
 //HTML endpoints********
 app.get('/', function homepage (req, res) {
-  res.sendFile(__dirname + './views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 //JSON API Endpoints*************
 
