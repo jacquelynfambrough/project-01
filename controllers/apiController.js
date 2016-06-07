46b4e6ea2b805042ef8b706a8e7b6b80ac432be4
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-06-07 11:17:44 -0700

    fixed syntax error in apiController

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,11 +1,11 @@
 function index(req, res) {
   res.json({
-    // message: "Project-01 Jacquelyn Fambrough"
-    // documentation_url: "https://github.com/jacquelynfambrough/project-01",
-    // base_url: "https://secure-badlands-39869.herokuapp.com/",
-    // endpoints: [
-    //  {method: "GET", path: "/api/", description: "Describes available endpoints"}
-    // ]
+    message: "Project-01 Jacquelyn Fambrough",
+    documentation_url: "https://github.com/jacquelynfambrough/project-01",
+    base_url: "https://secure-badlands-39869.herokuapp.com/",
+    endpoints: [
+     {method: "GET", path: "/api", description: "Describes available endpoints"}
+   ]
   });
 }
 
